e08b8c541d91d0bc2547e00cf62a3be1e345f4b8
Sergei Terentev <sergei.terentev@aspose.com>
2022-06-29 09:47:40 +0300

    Samples for SDK v22.6

---
+++ Examples/GroupDocs.Signature.Cloud.Examples/BasicUsage/GetLicenseConsumption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GroupDocs.Signature.Cloud.Sdk.Api;
+
+namespace GroupDocs.Signature.Cloud.Examples.CSharp
+{
+    /// <summary>
+    /// This example demonstrates how to get metered license consumption information
+    /// </summary>
+    public class GetLicenseConsumption
+    {
+        public static void Run()
+        {
+            var apiInstance = new LicenseApi(Constants.GetConfig());
+
+            try
+            {
+                var response = apiInstance.GetConsumptionCredit();
+
+                Console.WriteLine($"Credits: {response.Credit}");
+                Console.WriteLine($"Quantity: {response.Quantity}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception while calling LicenseApi: " + e.Message);
+            }
+        }
+    }
+}
